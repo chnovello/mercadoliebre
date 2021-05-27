07c8259c2362f0e12edc56a5c936e7db2813e846
Christian Novello <83038255+chnovello@users.noreply.github.com>
2021-05-27 20:20:24 -0300

    server remoto2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.resolve(__dirname,"./public")));
 
 app.set("port", process.env.PORT || 3000);
 
-app.listen(3000, () => console.log('open server in http://localhost:3000'));
+app.listen(3000, () => console.log('open server in http://localhost:'+app.get("port")));
 
 
 app.get("/",(req,res) => res.sendFile(path.join(__dirname, "views", "home.html")));
